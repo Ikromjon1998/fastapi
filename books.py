f05220383ad02354a46f184900979f89a06e3db8
Ikromjon <57859849+Ikromjon1998@users.noreply.github.com>
2025-06-01 11:41:48 +0200

    closed first project

--- books.py
+++ books.py
@@ -1,5 +1,5 @@
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 
 BOOKS = [
     {'title': 'Title One', 'author': 'Author One', 'category': 'science'},
@@ -25,5 +25,52 @@ async def read_book(book_title: str):
 
 @app.get("/books/")
 async def read_category_by_query(category: str):
-    books_to_return = [book for book in BOOKS if book.get('category').casefold() == category.casefold()]
+    books_to_return = []
+    for book in BOOKS:
+        if book.get('category').casefold() == category.casefold():
+            books_to_return.append(book)
     return books_to_return
+
+
+# Get all books from a specific author using path or query parameters
+@app.get("/books/byauthor/")
+async def read_books_by_author_path(author: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get('author').casefold() == author.casefold():
+            books_to_return.append(book)
+
+    return books_to_return
+
+
+@app.get("/books/{book_author}/")
+async def read_author_category_by_query(book_author: str, category: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get('author').casefold() == book_author.casefold() and \
+                book.get('category').casefold() == category.casefold():
+            books_to_return.append(book)
+
+    return books_to_return
+
+@app.post("/books/create_book")
+async def create_book(new_book: dict):
+    BOOKS.append(new_book)
+    return {"message": "Book created successfully", "book": new_book}
+
+
+@app.put("/books/update_book")
+async def update_book(updated_book=Body()):
+    for index, book in enumerate(BOOKS):
+        if book.get('title').casefold() == updated_book.get('title').casefold():
+            BOOKS[index] = updated_book
+            return {"message": "Book updated successfully", "book": updated_book}
+    return {"error": "Book not found for update"}
+
+@app.delete("/books/delete_book/{book_title}")
+async def delete_book(book_title: str):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get('title').casefold() == book_title.casefold():
+            deleted_book = BOOKS.pop(i)
+            return {"message": "Book deleted successfully", "book": deleted_book}
+    return {"error": "Book not found for deletion"}
