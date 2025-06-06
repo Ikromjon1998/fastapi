caccb043f0715a921437c64c02a6149ad2b6af6b
Ikromjon <57859849+Ikromjon1998@users.noreply.github.com>
2025-06-06 21:56:06 +0200

    added data validation

--- books2.py
+++ books2.py
@@ -1,4 +1,7 @@
+from typing import Optional
+
 from fastapi import FastAPI, Body
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 
@@ -16,6 +19,13 @@ class Book:
         self.description = description
         self.rating = rating
 
+class BookRequest(BaseModel):
+    id: Optional[int] = None
+    title: str = Field(min_length=1, max_length=100)
+    author: str = Field(min_length=1, max_length=100)
+    description: str = Field(min_length=1, max_length=500)
+    rating: int = Field(ge=1, le=5, description="Rating must be between 1 and 5")
+
 BOOKS = [
     Book(1, 'Computer Science Pro', 'codingwithroby', 'A very nice book!', 5),
     Book(2, 'Be Fast with FastAPI', 'codingwithroby', 'A great book!', 5),
@@ -30,7 +40,17 @@ BOOKS = [
 async def read_all_books():
     return BOOKS
 
-@app.post("/books")
-async def create_book(book_request=Body()):
-    BOOKS.append(book_request)
+def find_book_id(book: Book):
+    # if len(BOOKS) > 0:
+    #     book.id = BOOKS[-1].id + 1
+    # else:
+    #     book.id = 1
+    # return book
+    book.id = 1 if len(BOOKS) > 0 else BOOKS[-1].id + 1
+    return book
+
+@app.post("/create-book")
+async def create_book(book_request: BookRequest):
+    new_book = Book(**book_request.model_dump())
+    BOOKS.append(find_book_id(new_book))
     return
